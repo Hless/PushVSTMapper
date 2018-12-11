9699fd2cde72d5985c4c750d9e32ca1589adaba9
Boy van der Laak <boyvanderlaak@gmail.com>
2018-12-11 17:43:48 +0100

    Removing ubermap from remote repo

--- ubermap/rm_ignored_device_cfgs.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-pushd ~/Ubermap/Devices
-grep -rnwl . -e 'Ignore = True' > temp.txt
-awk '{gsub(/ /,"\\ ")}8' temp.txt > Ignored.txt
-rm temp.txt
-cat Ignored.txt | xargs rm
-rm Ignored.txt
-popd
